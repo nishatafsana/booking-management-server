dd4fdad86991a087e0dc38d4e1b6b9091a9abee5
nishatafsana <nishattasnimafsana539@gmail.com>
2024-10-17 20:07:54 +0600

    7

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000
 
 // middleware
@@ -185,7 +185,24 @@ app.get('/my-listings/:email',verifyToken,verifyHost, async (req, res) => {
 })
 
 
+ // 10.create-payment-intent
+ app.post('/create-payment-intent',verifyToken,async(req,res)=>{
+  const price =req.body.price;
+  const priceInCent = parseFloat(price) * 100
 
+  if (!price || priceInCent < 1) return
+
+  const { client_secret } = await stripe.paymentIntents.create({
+    amount: priceInCent,
+    currency: 'usd',
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+
+  })
+  res.send({ clientSecret: client_secret })
+ })
 
       // 7.get all users data from db..varifytoken andvarifay admin sobar last a add kora hoice jano /users diya data jakaw dekte na pare.................
       app.get('/users', verifyToken,verifyAdmin, async (req, res) => {
@@ -212,7 +229,196 @@ app.get('/my-listings/:email',verifyToken,verifyHost, async (req, res) => {
     res.send(result)
   })
 
+    //11. Save a booking data in db
+  app.post('/booking',verifyToken,async(req,res)=>{
+    const bookingData = req.body
+    const result = await bookingsCollection.insertOne(bookingData)
+ 
+    res.send(result)
 
+  })
+  
+ 
+   //12. update Room Status
+   app.patch('/room/status/:id', async (req, res) => {
+    const id = req.params.id
+    const status = req.body.status
+    // change room availability status
+    const query = { _id: new ObjectId(id) }
+    const updateDoc = {
+      $set: { booked: status },
+    }
+    const result = await roomsCollection.updateOne(query, updateDoc)
+    res.send(result)
+  })
+
+
+// get all booking for guest
+app.get('/my-bookings/:email',verifyToken,async(req,res)=>{
+const email=req.params.email
+const query={'guest.email':email}
+const result=await bookingsCollection.find(query).toArray()
+res.send(result)
+})
+// manage booking host
+app.get('/manage-bookings/:email',verifyToken,verifyHost,async(req,res)=>{
+const email=req.params.email
+const query={'host.email':email}
+const result=await bookingsCollection.find(query).toArray()
+res.send(result)
+})
+
+
+
+ // Admin Statistics
+ app.get('/admin-stat', verifyToken, verifyAdmin, async (req, res) => {
+  const bookingDetails = await bookingsCollection
+    .find(
+      {},
+      {
+        projection: {
+          date: 1,
+          price: 1,
+        },
+      }
+    )
+    .toArray()
+
+  const totalUsers = await usersCollection.countDocuments()
+  const totalRooms = await roomsCollection.countDocuments()
+  const totalPrice = bookingDetails.reduce(
+    (sum, booking) => sum + booking.price,
+    0
+  )
+  // const data = [
+  //   ['Day', 'Sales'],
+  //   ['9/5', 1000],
+  //   ['10/2', 1170],
+  //   ['11/1', 660],
+  //   ['12/11', 1030],
+  // ]
+  const chartData = bookingDetails.map(booking => {
+    const day = new Date(booking.date).getDate()
+    const month = new Date(booking.date).getMonth() + 1
+    const data = [`${day}/${month}`, booking?.price]
+    return data
+  })
+  chartData.unshift(['Day', 'Sales'])
+  // chartData.splice(0, 0, ['Day', 'Sales'])
+
+  console.log(chartData)
+
+  console.log(bookingDetails)
+  res.send({
+    totalUsers,
+    totalRooms,
+    totalBookings: bookingDetails.length,
+    totalPrice,
+    chartData,
+  })
+})
+
+
+// Host Statistics
+app.get('/host-stat', verifyToken, verifyHost, async (req, res) => {
+  const { email } = req.user
+  const bookingDetails = await bookingsCollection
+    .find(
+      { 'host.email': email },
+      {
+        projection: {
+          date: 1,
+          price: 1,
+        },
+      }
+    )
+    .toArray()
+
+  const totalRooms = await roomsCollection.countDocuments({
+    'host.email': email,
+  })
+  const totalPrice = bookingDetails.reduce(
+    (sum, booking) => sum + booking.price,
+    0
+  )
+  const { timestamp } = await usersCollection.findOne(
+    { email },
+    { projection: { timestamp: 1 } }
+  )
+
+  const chartData = bookingDetails.map(booking => {
+    const day = new Date(booking.date).getDate()
+    const month = new Date(booking.date).getMonth() + 1
+    const data = [`${day}/${month}`, booking?.price]
+    return data
+  })
+  chartData.unshift(['Day', 'Sales'])
+  // chartData.splice(0, 0, ['Day', 'Sales'])
+
+  console.log(chartData)
+
+  console.log(bookingDetails)
+  res.send({
+    totalRooms,
+    totalBookings: bookingDetails.length,
+    totalPrice,
+    chartData,
+    hostSince: timestamp,
+  })
+})
+
+  // Guest Statistics
+  app.get('/guest-stat', verifyToken, async (req, res) => {
+    const { email } = req.user
+    const bookingDetails = await bookingsCollection
+      .find(
+        { 'guest.email': email },
+        {
+          projection: {
+            date: 1,
+            price: 1,
+          },
+        }
+      )
+      .toArray()
+
+    const totalPrice = bookingDetails.reduce(
+      (sum, booking) => sum + booking.price,
+      0
+    )
+    const { timestamp } = await usersCollection.findOne(
+      { email },
+      { projection: { timestamp: 1 } }
+    )
+
+    const chartData = bookingDetails.map(booking => {
+      const day = new Date(booking.date).getDate()
+      const month = new Date(booking.date).getMonth() + 1
+      const data = [`${day}/${month}`, booking?.price]
+      return data
+    })
+    chartData.unshift(['Day', 'Sales'])
+    // chartData.splice(0, 0, ['Day', 'Sales'])
+
+    console.log(chartData)
+
+    console.log(bookingDetails)
+    res.send({
+      totalBookings: bookingDetails.length,
+      totalPrice,
+      chartData,
+      guestSince: timestamp,
+    })
+  })
+  
+// 5.delete a booking(gust korve)..hoi nai...
+
+app.delete('/booking/:id', verifyToken, async (req, res) => {
+  const id = req.params.id
+  const query = { _id: new ObjectId (id) }
+  const result = await bookingsCollection.deleteOne(query)
+  res.send(result)
+})
 
 
     // Send a ping to confirm a successful connection
